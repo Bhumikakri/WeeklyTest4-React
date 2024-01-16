767a9744a6de16a7c57b8325bd4cc63f2ef6dae8
unknown <bhumikakri2002@gmail.com>
2024-01-16 21:37:55 +0530

    Add functionality were left

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+import Main from './Component/Main/Main';
+import ShowInformation from './Component/ShowData/retriveInfo';
+import AddPerson from './Component/AddPerson/AddPerson';
 
 function App() {
+  const routes=createBrowserRouter([
+    {
+      path:"/",
+      element:<Main />,
+      children:[
+        {
+          path:"/",
+          element:<AddPerson />,
+        },
+        {
+          path:"/retrieve",
+          element:<ShowInformation />,
+        }
+      ]
+    }
+  ])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RouterProvider router={routes}>
+      </RouterProvider>
     </div>
   );
 }
